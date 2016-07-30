106c9ec222a412e4d2a6bef8c422f3f368d4342b
cupko55 <akrizmani@tvz.hr>
2016-07-30 17:52:12 +0200

    seed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -205,72 +205,37 @@
                 map: map
             });
         }
-        function setMarkers(map) {
-            // Adds markers to the map.
-
-            // Marker sizes are expressed as a Size of X,Y where the origin of the image
-            // (0,0) is located in the top left of the image.
-
-            // Origins, anchor positions and coordinates of the marker increase in the X
-            // direction to the right and in the Y direction down.
-            var image = {
-                url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-                // This marker is 20 pixels wide by 32 pixels high.
-                size: new google.maps.Size(20, 32),
-                // The origin for this image is (0, 0).
-                origin: new google.maps.Point(0, 0),
-                // The anchor for this image is the base of the flagpole at (0, 32).
-                anchor: new google.maps.Point(0, 32)
-            };
-            // Shapes define the clickable region of the icon. The type defines an HTML
-            // <area> element 'poly' which traces out a polygon as a series of X,Y points.
-            // The final coordinate closes the poly by connecting to the first coordinate.
-            var shape = {
-                coords: [1, 1, 1, 20, 18, 20, 18, 1],
-                type: 'poly'
-            };
-            for (var i = 0; i < beaches.length; i++) {
-                var beach = beaches[i];
-                var marker = new google.maps.Marker({
-                    position: {lat: beach[1], lng: beach[2]},
-                    map: map,
-                    icon: image,
-                    shape: shape,
-                    title: beach[0],
-                    zIndex: beach[3]
-                });
-            }
-            // Brisanje markera.
-            function deleteMarkers() {
-                marker.setMap(null);
-                marker = null;
-            }
-
-            /*Dohvaca cijene sa servera*/
-            function getPrice() {
-                distance = distance / 1000;
-                distance = distance.toFixed(2);
-                $.ajax({
-                    type: 'GET',
-                    url: "{{ url('order/getPrice') }}/" + distance,
-                    success: function (data) {
-                        var cijene = data;
-                        document.getElementById("cjene").innerHTML = "<p><b>Cijena</b></p>";
-                        for (i = 0; i < cijene.length; i++) {
-                            document.getElementById("cjene").innerHTML += "<p>" + cijene[i].name + " " + cijene[i].price + " kn</p>";
-                        }
-                        $('#cijene').show();
-                        $('.saznaj-cijenu').hide();
-                    }
-                });
-            }
-
-            function getAlert() {
-                var distanceLeng = $("#output").text().length - 3;
-                var distance = $("#output").text();
-                var dist = distance.substring(0, distanceLeng);
-                document.cookie = "distance=" + dist;
-            }
+        // Brisanje markera.
+        function deleteMarkers() {
+            marker.setMap(null);
+            marker = null;
         }
+
+        /*Dohvaca cijene sa servera*/
+        function getPrice() {
+            distance = distance / 1000;
+            distance = distance.toFixed(2);
+            $.ajax({
+                type: 'GET',
+                url: "{{ url('order/getPrice') }}/" + distance,
+                success: function (data) {
+                    var cijene = data;
+                    document.getElementById("cjene").innerHTML = "<p><b>Cijena</b></p>";
+                    for (i = 0; i < cijene.length; i++) {
+                        document.getElementById("cjene").innerHTML += "<p>" + cijene[i].name + " " + cijene[i].price + " kn</p>";
+                    }
+                    $('#cijene').show();
+                    $('.saznaj-cijenu').hide();
+                }
+            });
+        }
+
+        function getAlert() {
+            var distanceLeng = $("#output").text().length - 3;
+            var distance = $("#output").text();
+            var dist = distance.substring(0, distanceLeng);
+            document.cookie = "distance=" + dist;
+        }
+
     </script>
 @endsection
